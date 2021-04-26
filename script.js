17ea6fe2531a7e01b7a3f5117189734a4d2d00c5
Kaloyan <kaloyankulov2003kk@gmail.com>
2021-04-27 00:33:30 +0300

    Added some functionalities

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function addTask() {
     let taskText = document.getElementById("taskTextID");
 
     if (taskText.value != "") {
-        taskContainer.innerHTML += "<div class='shadow-md p-2 border --task' id=" 
-        + "task" + counter + ">" 
-        + taskText.value + "<button class='deleteButton' onclick='showIt(this)'>Delete</button></div>";
+        taskContainer.innerHTML += "<div class='shadow-md p-2 border --task' id="
+            + "task" + counter + ">"
+            + taskText.value + "<button class='deleteButton' onclick='deleteTask(this)'>Delete</button></div>";
         counter++;
         taskText.value = null;
     } else {
@@ -15,15 +15,24 @@ function addTask() {
     }
 }
 
-// function deleteTask() {
-//     let taskIndex = showIt(docu)
-// }
-
-function showIt(element) {
+function deleteTask(element) {
     let parent = element.parentNode;
     let taskToDelete = document.getElementById(parent.id)
     taskToDelete.parentNode.removeChild(taskToDelete)
-    // alert(parent.id);
-    // alert(content.id);
     console.log(parent.id)
-  }
+}
+
+$(document).ready(function () {
+    $('#taskTextID').keypress(function (e) {
+        if (e.keyCode == 13)
+            $('#buttonAddID').click();
+    });
+});
+
+function refreshTime() {
+    let timeDisplay = document.getElementById("time");
+    let dateString = new Date().toLocaleString("en-US", { timeZone: "America/Sao_Paulo" });
+    let formattedString = dateString.replace(", ", " - ");
+    timeDisplay.innerHTML = formattedString;
+    setInterval(refreshTime, 1000);
+}
